61603f6e827733078ff824e04ce1571618e46b55
huhubun <nongzc@outlook.com>
2020-09-13 00:45:27 +0800

    Add `provider set`, `provider clear` commands

--- src/DDNSSharp/Commands/ProviderCommand.cs
+++ src/DDNSSharp/Commands/ProviderCommand.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using DDNSSharp.Commands.ProviderCommands;
+using McMaster.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DDNSSharp.Commands
 {
+    [Subcommand(
+        typeof(SetCommand),
+        typeof(ClearCommand)
+    )]
     class ProviderCommand
     {
     }
