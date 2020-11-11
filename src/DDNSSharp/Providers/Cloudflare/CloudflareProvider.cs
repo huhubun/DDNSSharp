09b9c12fec3370cdc81ed0e220c98932446ddeac
huhubun <nongzc@outlook.com>
2020-11-12 00:07:45 +0800

    chore: 隐藏未实现的 cloudflare provider

--- src/DDNSSharp/Providers/Cloudflare/CloudflareProvider.cs
+++ src/DDNSSharp/Providers/Cloudflare/CloudflareProvider.cs
@@ -4,17 +4,17 @@ using McMaster.Extensions.CommandLineUtils;
 
 namespace DDNSSharp.Providers.Cloudflare
 {
-    [Provider(Name = "cloudflare")]
-    class CloudflareProvider : ProviderBase
-    {
-        public CloudflareProvider(CommandLineApplication app)
-        {
-            App = app;
-        }
+    //[Provider(Name = "cloudflare")]
+    //class CloudflareProvider : ProviderBase
+    //{
+    //    public CloudflareProvider(CommandLineApplication app)
+    //    {
+    //        App = app;
+    //    }
 
-        public override void Sync(SyncContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+    //    public override void Sync(SyncContext context)
+    //    {
+    //        throw new System.NotImplementedException();
+    //    }
+    //}
 }
